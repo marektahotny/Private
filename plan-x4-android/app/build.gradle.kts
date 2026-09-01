0e5eea55e1d0c9aaa48ef0cd5be4d2b0f1025aab
marektahotny <marek.tahotny1@gmail.com>
2026-09-01 21:11:25 +0200

    Replace material-icons-outlined with material-icons-extended

--- plan-x4-android/app/build.gradle.kts
+++ plan-x4-android/app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-outlined")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
